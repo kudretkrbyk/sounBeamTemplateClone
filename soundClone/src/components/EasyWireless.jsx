a3c56518f666f8748cd3c0b15ae465a433b2d5f8
kudretkrbyk <kudretkrbyk@gmail.com>
2024-05-29 15:07:33 +0300

    .

--- soundClone/src/components/EasyWireless.jsx
+++ soundClone/src/components/EasyWireless.jsx
@@ -5,11 +5,8 @@ import { IoHeadsetOutline } from "react-icons/io5";
 
 export default function EasyWireless() {
   return (
-    <div className="relative w-full h-[700px] overflow-y-scroll">
-      <div
-        data-speed="0.25"
-        className="parallax z-30 absolute w-full h-[700px] bg-cover bg-center bg-white bg-[url(https://static.wixstatic.com/media/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg/v1/fill/w_1265,h_772,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg)]"
-      ></div>
+    <div className="relative w-full h-[700px] ">
+      <div className="parallax z-30 absolute w-full h-[700px] bg-cover bg-center bg-white bg-[url(https://static.wixstatic.com/media/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg/v1/fill/w_1265,h_772,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg)]"></div>
       <div
         className="absolute bg-black bg-opacity-50 w-full h-[700px] top-0 left-0 z-40"
         data-speed="0.4"
