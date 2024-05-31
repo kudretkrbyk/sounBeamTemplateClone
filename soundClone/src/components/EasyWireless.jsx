9a4cc422c235915765f1231233a8512905f33a3f
kudretkrbyk <kudretkrbyk@gmail.com>
2024-05-31 11:33:42 +0300

    parallax

--- soundClone/src/components/EasyWireless.jsx
+++ soundClone/src/components/EasyWireless.jsx
@@ -6,11 +6,11 @@ import { Parallax } from "react-scroll-parallax";
 
 export default function EasyWireless() {
   return (
-    <div className="relative w-full h-[700px] overflow-hidden  ">
-      <Parallax speed={-9}>
+    <div className="relative w-full h-[800px] overflow-hidden  ">
+      <Parallax speed={-20}>
         {" "}
-        <div className="-mt-24 parallax z-30 absolute w-full h-[900px] bg-cover bg-center bg-white bg-[url(https://static.wixstatic.com/media/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg/v1/fill/w_1265,h_772,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg)]"></div>
-        <div className="-mt-24 absolute bg-black bg-opacity-50 w-full h-[900px] top-0 left-0 z-40"></div>
+        <div className="-mt-60  parallax z-30 absolute w-full h-[900px] bg-cover bg-no-repeat bg-center bg-white bg-[url(https://static.wixstatic.com/media/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg/v1/fill/w_1265,h_772,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/82fcd3_0d16229674fd4698b2a7a99a34d42de1~mv2.jpg)]"></div>
+        <div className="-mt-60 absolute bg-black bg-opacity-50 w-full h-[800px] top-0 left-0 z-40"></div>
       </Parallax>
       <Parallax></Parallax>
 
